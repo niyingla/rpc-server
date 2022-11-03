3409e3817424f85aaffbaed305cb83e00f9a9e5b
huohuo <as1314520>
2022-11-03 20:47:46 +0800

    生成对象方式改成definition

--- rpc-com/src/main/java/com/example/demo/netty/config/RpcSource.java
+++ rpc-com/src/main/java/com/example/demo/netty/config/RpcSource.java
@@ -19,25 +19,6 @@ public class RpcSource {
 
     private String serverName;
 
-    private String[] commonReqAspect = new String[]{};;
-
-    private String[] commonRecAspect = new String[]{};
-
-    public String[] getCommonReqAspect() {
-        return commonReqAspect;
-    }
-
-    public void setCommonReqAspect(String[] commonReqAspect) {
-        this.commonReqAspect = commonReqAspect;
-    }
-
-    public String[] getCommonRecAspect() {
-        return commonRecAspect;
-    }
-
-    public void setCommonRecAspect(String[] commonRecAspect) {
-        this.commonRecAspect = commonRecAspect;
-    }
 
     public Integer getConnectCount() {
         return connectCount;
